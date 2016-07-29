93b64cf202cf17cfa4c1dff1271aa4386029cc47
yutackall <yutackall@gmail.com>
2016-07-29 13:30:49 +0900

    Building the Application Shell

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' }
+    ]);
+
+    this.router = router;
   }
 }
